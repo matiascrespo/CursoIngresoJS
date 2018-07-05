cb1f1940e2d588ef89b4940468e3013974e83e1f
matiascrespo <40859037+matiascrespo@users.noreply.github.com>
2018-07-05 11:24:54 -0300

    Primer Commit
    Estos son los 4 ejercicios de la primera clase.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,15 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	//var person = prompt("Please enter your name", "Harry Potter"); Esto lo saque de W3School
+	/*
+	Esto es un comentario de bloque
+	*/
+	var nombre;
+	//nombre="Matias";
+	//alert(nombre);
+	nombre=prompt("Ingrese su nombre");
+	alert("Su nombre es: "+nombre);
 	
 }
 
