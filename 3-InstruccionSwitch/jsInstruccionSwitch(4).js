651f6ee46c0048d8d0648589aeca27e6bd37ae1d
matiascrespo <40859037+matiascrespo@users.noreply.github.com>
2018-07-13 12:23:37 -0300

    Ejercicios Switch 1 al 4
    Switch / Defalut/ Break
    
    etc

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -6,7 +6,28 @@ var mesDelAño = document.getElementById('mes').value;
 //alert (mesDelAño);
 	
 	
+switch(mesDelAño)
+{
+	case "Febrero":
+	alert("Tiene 28 dias");
+	break;
 
+	case "Enero":
+	case "Marzo":
+	case "Mayo":
+	case "Julio":
+	case "Agosto":
+	case "Octubre":
+	case "Agosto":
+	case "Octubre":
+	case "Diciembre":
+	alert("Tiene 31 dìas.");
+	break;
+
+	default:
+	alert("Tiene 30 dìas");
+	break;
+}
 
 
 }//FIN DE LA FUNCIÓN
