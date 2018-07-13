0c7d7b216939414e718a7eb0cd7c7c5b1da498fc
matiascrespo <40859037+matiascrespo@users.noreply.github.com>
2018-07-13 12:25:11 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,51 @@
+/*
+	Enumerados... datos que ya sabemos que existen: lunes, martes, etc
+	switch: me hace mas legible el codigo y puedo hacer mas funcional el codigo
+	SWITCH TEMA DE A
+	RESPETAR UNA VARIABLE PARA COMAPRAR
+	Y A ESA VARIALE IR TOMANDO LOS VALORES Y RESPETARLOS.
+
+	SWITCH(CONJUNTO DE IF)
+
+	
+	switch(dia)
+	{
+		case "Lunes":
+			alert("Lunes");
+			break;
+
+		case "sabado":
+
+
+
+	}
+*/
 function mostrar()
 {
+	var dia;
+	var lunes;
+	var martes;
+	var miercoles;
+	var jueves;
+	var viernes;
+	var sabado;
+	var domingo;
+	var finDeSemana;
+	var diaHabil;
+	
+	finDeSemana=sabado&&domingo;
+	
+
+	dia=prompt("Ingrese dia de la semana");
+	diaHabil<6
+	finDeSemana>5
+
+	if(finDeSemana)
+		{
+
+		}
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,42 @@
+
 function mostrar()
 {
 
+	var numeroUno;
+	var numeroDos;
+ 	var resultado;
+ 	var suma;
+ 	var division;
+
+	numeroUno=prompt("ingrese el primer numero");
+	numeroDos=prompt("ingrese el segundo numero");
+	
+	suma=numeroUno+numeroDos;
+	division=numeroUno/numeroDos;
+	
+	if(numeroUno==numeroDos)
+		{
+			alert(numeroUno+numeroDos)
+		}
+
+		
+	if(numeroUno>numeroDos)
+		{
+			numeroUno=parseInt(numeroUno);
+			numeroDos=parseInt(numeroDos);
+			resultado=division
+		}
+
+		else
+			{
+				resultado=suma;	
+			}
+
+	if(suma<50)
+		{
+			resultado=suma;
+			alert("La suma es "+suma+" y es menor a 50");
+		}
+
+	
 }
