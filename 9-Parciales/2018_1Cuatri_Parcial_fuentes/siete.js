85f371ecf6ef1d3e09f40afe52b3db3e26896b56
matiascrespo <40859037+matiascrespo@users.noreply.github.com>
2018-07-27 12:26:41 -0300

    Parcial 7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,46 @@
+//
+
 function mostrar()
 {
+	var sexo;
+	var edad;
+	var nombre;
+
+	
+	contador=0;
+
+	while(contador<3)
+	{
+
+		edad=prompt("Ingrese su edad.");
+		edad=parseInt(edad);
+		while(edad<0 || edad>100)
+		{
+			edad=prompt("Ingrese una edad entre 0 y 100 años.");
+		}
+
+		sexo=prompt("Ingrese su sexo");
+		
+		while(sexo!="f" && sexo!="m")
+		{
+			sexo=prompt("Ingrese un sexo valido.");
+		}
+
+		nombre=prompt("Ingrese su nombre");
+
+		contador++;
+
+	}
+
+
+	
+	
+		
+		
+
+
+
+
+
 
 }
