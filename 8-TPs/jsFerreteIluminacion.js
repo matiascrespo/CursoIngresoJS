b0046f95e0d1b61b9f2ec290f373bdc55aa35863
matiascrespo <40859037+matiascrespo@users.noreply.github.com>
2018-07-26 22:30:52 -0300

    Ferrete IF

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,4 +1,4 @@
-/*4.	Para el departamento de iluminación:
+	/*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
@@ -7,47 +7,96 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
-
-primero tomo los datos
-primero catnidad de lamparas
-desues descuento(dentro if)
-
-Precio despues de todos los IF (UNA SOLA VEZ)
-
  */
 function CalcularPrecio () 
 {
- 	
 	
-	var precio;
-	var cantidadlamparas;
-	var marca;
+	var precioUnitario;
+	var precioFinal;
+	var cantidad;
 	var precioBruto;
-	var descuento;
-	var precioDescuento;
-	var descuento;
+	var marca;
+	var iibb;
+	
 
-	precio=35
 
-	cantidadlamparas=document.getElementById('Cantidad').value;
+	precioUnitario=35;
+	cantidad=document.getElementById('Cantidad').value;
+	cantidad=parseInt(cantidad);
+	precioBruto=precioUnitario*cantidad
 	marca=document.getElementById('Marca').value;
-	precioDescuento=precio*descuento
-	document.getElementById('precioDescuento').value=precioDescuento;
+	precioFinal=parseInt(precioFinal);
 
-	if(cantidadlamparas>6 && marca)
+	switch(cantidad)
+	{
+		case 1:
+		precioFinal=precioUnitario;
+		break;
+
+		case 2:
+		precioFinal=precioUnitario*2;
+		break;
+
+		case 5:
+		switch(marca)
 		{
-			descuento=precio*0.5;
-		}
+			case "ArgentinaLuz":
+			precioFinal=precioBruto*0.6;
+			break;
 
-		else
+			default:
+			precioFinal=precioBruto*0.7;
+			break;
+		
+		
+		case 4:
+		switch(marca)
 		{
-			precio=precio
+			case "ArgentinaLuz":
+			case "FelipeLamparas":
+			precioFinal=precioBruto*0.75;
+			break;
+
+			default:
+			precioFinal=precioBruto*0.8;
+			break;
+		
+		
+
+		case 3:
+		switch(marca)
+		{
+			case "ArgentinaLuz":
+			precioFinal=precioBruto*0.85;
+			break;
+
+			case "FelipeLamparas":
+			precioFinal=precioBruto*0.9;
+			break;
+
+			default:
+			precioFinal=precioBruto*0.95;
+			break;
 		}
+		
+		case 6:
+		precioFinal=precioBruto*0.5;
+		break;
+
+		default:
+		precioFinal=precioBruto*0.5;
+		break;
+		
+		}
+	}
+	}
 
 
 
 
+	document.getElementById('precioDescuento').value=precioFinal;
 
 
 
 }
+
